abde8a3730b07cc10a6d611a67ac942bc2b91b53
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-22 16:40:36 -0400

    added zip and info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Hangman from './Hangman';
 
-function App() {
-  return (
-    <div className="App">
-      HELLO THERE!
-    </div>
-  );
-}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Hangman />
+      </div>
+    );
+  }
+
+  }
 
 export default App;
